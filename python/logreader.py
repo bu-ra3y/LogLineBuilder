96e38c13c239f3f9a22ff9c2abdd397cb6e403e8
Will Barley <wbarley@wbarley-mbp.dyn.yojoe.local>
2015-07-25 18:45:21 +0100

    updated Python module - can now plot data read from a file; also allows you to break up the metric series into several plots

--- python/logreader.py
+++ python/logreader.py
@@ -4,6 +4,7 @@
 #  
 #
 from collections import defaultdict
+import matplotlib.pyplot as plt
 
 
 
@@ -41,3 +42,40 @@ def readInput(inputFile):
 # Return all of the data
 def getData():
   return data
+
+# Chart the data using MatPlotLib
+def plotData(config):
+  # config contains 1 item for each intended subplot.
+  #  thus, the length of config should tell us how many subplots we want
+  num_subplots = len(config)
+  
+  # Create a figure with several subplots
+  #  number of subplots according to the config
+  #  all subplots share the temporal axis (x)
+  figure, axes_array = plt.subplots(num_subplots, sharex=True)
+  
+  for key in data.keys():
+    # grab the data values for this series
+    y = data[key]
+  
+    # we have no x-axis reference, so 
+    #  we will simply plot each value incrementally
+    #  along the x-axis, starting at 0, going up
+    #  to len(y)
+    x = range(len(y))
+  
+    # Now we need to use the config to plot
+    #  this data on the correct subplot/axes
+    # go through each item/plot in the config list
+    for i in range(len(config)):
+      if key in config[i]:
+        # if this series belongs in this plot
+        #  then plot it on the right axes
+        axes_array[i].plot(x, y, label=key)
+  
+  # Activate each legend
+  for axes in axes_array:
+    axes.legend()  
+
+  # Display
+  plt.show()
--- python/logreadertest.py
+++ python/logreadertest.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 # test logreader
-
-
 import logreader
 
 logreader.readInput('test/1.data')
 d = logreader.getData()
+
+# print out the names of the series we have
 print(d.keys())
+
+# print out all the data
+print(d)
+
+# divide up the series into 2 plots:
+#   first: z, y, m, x, dist
+#   second: pan, tilt
+config=[['z','y','m','x','dist'], ['pan','tilt']]
+
+# plot it!
+logreader.plotData(config)
